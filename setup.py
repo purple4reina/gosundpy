8e20b268290f15ff12673cfb27c0d07c987e5c25
Rey Abolofia <purple4reina@gmail.com>
2022-10-16 00:29:20 -0700

    Properly get version.

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
+import codecs
+import os.path
+
 from setuptools import find_packages, setup
 
-from gosundpy import __version__
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('version'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 setup(
     name="gosundpy",
-    version=__version__,
+    version=get_version("gosundpy/version.py"),
     url="https://github.com/purple4reina/gosundpy",
     author="Rey Abolofia",
     author_email="purple4reina@gmail.com",
